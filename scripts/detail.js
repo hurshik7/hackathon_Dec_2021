8951f971d35aac82ea78172d7260bfb4e47a7d43
hurshik7 <hurshik7@gmail.com>
2021-12-19 01:02:37 -0800

    feat: add like functions

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,20 +1,52 @@
 let icon = document.getElementById("like-icon");
-let count = 0;
+let is_like_global = false;
+
+function fillLikeIcon() {
+    icon.style.color = "#cf7585";
+    is_like_global = true;
+}
+
+function unfillLikeIcon() {
+    icon.style.color = "#31393c";
+    is_like_global = false;
+}
+
+function addItemToLikeList() {
+    let docIdFromLocal = localStorage.getItem("documentId");
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            userLikesCol = db.collection("likes").doc(user.uid);
+            userLikesCol.update({
+                likeList: firebase.firestore.FieldValue.arrayUnion(docIdFromLocal)
+            });
+        }
+    });
+}
+
+function deleteItemFromLikeList() {
+    let docIdFromLocal = localStorage.getItem("documentId");
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            userLikesCol = db.collection("likes").doc(user.uid);
+            userLikesCol.update({
+                likeList: firebase.firestore.FieldValue.arrayRemove(docIdFromLocal)
+            });
+        }
+    });
+}
+
 icon.addEventListener("click", function(){
-    if(count == 0){
-        icon.style.color = "#cf7585";
-        count = 1;
-        console.log("Like");
-    } else{
-        icon.style.color = "#31393c";
-        count = 0;
-        console.log("Unlike");
+    if(is_like_global == false){
+        fillLikeIcon();
+        addItemToLikeList();
+    } else {
+        unfillLikeIcon();
+        deleteItemFromLikeList();
     }
 })
 
 function showData() {
     let docIdFromLocal = localStorage.getItem("documentId");
-    //console.log(docIdFromLocal);
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             db.collection("posts").doc(docIdFromLocal).get()
@@ -34,6 +66,15 @@ function showData() {
 
                     document.getElementsByClassName("user-kandy-count")[0].innerText = user_kandy;
                     document.getElementsByClassName("user-id")[0].innerText = user_name;
+                })
+                var userLikes = db.collection("likes").doc(user_uid);
+                userLikes.get().then((likes) => {
+                    var likesList = likes.data();
+                    for (let i = 0; i < likesList.likeList.length; i++) {
+                        if (likesList.likeList[i] == docIdFromLocal) {
+                            fillLikeIcon();
+                            console.log("found!");                        }
+                    }
                 })
 
                 document.getElementsByClassName("item-image")[0].style.backgroundImage = `url(${sto_url})`;
